d66bb0a1fb643e367969605a196b9159a271f580
Samtyrio <ayoolasamuel128@gmail.com>
2023-09-08 20:15:28 -0700

    betty

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,32 +9,24 @@
 int main(void)
 {
 	int i, j;
-	int a, b, c, d;
 
 	for (i = 0; i < 100; i++)
 	{
-		a = i / 10;
-		b = i % 10;
-		for (j = 0; j < 100; j++)
+		for (j = i; j < 100; j++)
 		{
-			c = j / 10;
-			d = j % 10;
-			if (a < c || (a == c && b < d))
-			{
-				putchar(a + '0');
-				putchar(b + '0');
-				putchar(' ');
-				putchar(c + '0');
-				putchar(d + '0');
-				putchar(',');
+			int num1 = i / 10;
+			int num2 = i % 10;
+			int num3 = j / 10;
+			int num4 = j % 10;
 
-				if (a != 9 && b != 8 && c != 9 && d != 9)
-				{
-					putchar(' ');
-				}
-			}
+			putchar('0' + num1);
+			putchar('0' + num2);
+			putchar(' ');
+			putchar('0' + num3);
+			putchar('0' + num4);
+			putchar(',');
+			putchar(' ');
 		}
 	}
-	putchar('\n');
 	return (0);
 }

486175865e95506b64f69038ba2683dfdf2a7f2d
Samtyrio <ayoolasamuel128@gmail.com>
2023-09-12 12:57:54 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - this program uses _putchar
+ * it is written by me
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}

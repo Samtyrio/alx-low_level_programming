72de6d7af93ba10a1aefbf82ce8528e1b65e5e5b
Samtyrio <ayoolasamuel128@gmail.com>
2023-09-12 18:51:22 -0700

    tired

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this program print alphabets
+ */
+
+void print_alphabet(void)
+{
+	char apabet;
+
+	for (apabet = 'a'; apabet <= 'z'; apabet++)
+		_putchar(apabet);
+
+	_putchar('\n');
+}

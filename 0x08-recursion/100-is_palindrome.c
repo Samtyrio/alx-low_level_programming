f082f1d1f1c5808b94d7ef963a347c7ad5a44c0d
Samtyrio <ayoolasamuel128@gmail.com>
2023-09-28 15:24:37 -0700

    done

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints the length of a string
+ * @s: the pointer to the string
+ * Return: whatever is needed
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
+
+/**
+ * comp_rec - fills memory with a constant byte.
+ * @s: first bytes of the memory
+ * @ln: first bytes of the memory
+ * @i: first bytes of the memory
+ * Return: -
+ */
+int comp_rec(char *s, int ln, int i)
+{
+	if (i >= ln)
+	{
+		return (1);
+	}
+	if (*(s + i) != *(s + (ln - 1)))
+	{
+		return (0);
+	}
+	return (comp_rec(s, ln - 1, i + 1));
+}
+
+/**
+ * is_palindrome - fills memory with a constant byte.
+ * @s: first bytes of the memory
+ * Return: -
+ */
+int is_palindrome(char *s)
+{
+	int ln = _strlen_recursion(s);
+	int i = 0;
+
+	return (comp_rec(s, ln, i));
+}

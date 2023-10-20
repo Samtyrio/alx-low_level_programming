1675befdec9926b995a1ca7b2144188bd2ef6231
Samtyrio <ayoolasamuel128@gmail.com>
2023-10-20 01:14:59 -0700

    task0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints a list
+ * @h: the pointer to the first node
+ * Return: what is needed
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h -> str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else if (h -> str != NULL)
+		{
+			printf("[%d] %s\n", h -> len, h -> str);
+		}
+		nodes++;
+		h = h -> next;
+	}
+	return (nodes);
+}
